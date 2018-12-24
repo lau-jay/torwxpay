df0ae30415099235e295219c58dd175f1b504f70
pyclear <cappyclear@gmail.com>
2018-12-24 14:16:26 +0800

    refactor util

--- wxpay/utils.py
+++ wxpay/utils.py
@@ -5,64 +5,66 @@ from urllib.parse import quote
 from xml.etree import ElementTree
 
 
-def random_str(length=16):
-    chars = ''.join([string.ascii_letters, string.digits])
-    sa = []
-    for _ in range(length):
-        sa.append(random.choice(chars))
-    return ''.join(sa)
+class WeChat:
+    @staticmethod
+    def random_str(length=16):
+        chars = ''.join([string.ascii_letters, string.digits])
+        sa = []
+        for _ in range(length):
+            sa.append(random.choice(chars))
+        return ''.join(sa)
 
+    @staticmethod
+    def format_query_param(params, *, is_urlencode=False):
+        """
+        order params
+        >>> params = {'appid': 'wxd930ea5d5a258f4f', 'mch_id': '10000100', 'device_info': 1000,
+        ... 'body': 'test', 'nonce_str': 'ibuaiVcKdpRxkhJA'}
+        >>> WeChat.format_query_param(params)
+        'appid=wxd930ea5d5a258f4f&body=test&device_info=1000&mch_id=10000100&nonce_str=ibuaiVcKdpRxkhJA'
+        """
+        ordered_params = sorted(params)
+        tmp = []
+        for k in ordered_params:
+            v = quote(params[k]) if is_urlencode else params[k]
+            tmp.append("{0}={1}".format(k, v))
+        return "&".join(tmp)
 
-def format_query_param(params, *, is_urlencode=False):
-    """
-    order params
-    >>> params = {'appid': 'wxd930ea5d5a258f4f', 'mch_id': '10000100', 'device_info': 1000,
-    ... 'body': 'test', 'nonce_str': 'ibuaiVcKdpRxkhJA'}
-    >>> format_query_param(params)
-    'appid=wxd930ea5d5a258f4f&body=test&device_info=1000&mch_id=10000100&nonce_str=ibuaiVcKdpRxkhJA'
-    """
-    ordered_params = sorted(params)
-    tmp = []
-    for k in ordered_params:
-        v = quote(params[k]) if is_urlencode else params[k]
-        tmp.append("{0}={1}".format(k, v))
-    return "&".join(tmp)
+    @staticmethod
+    def gen_sign(params, *, sign_type='MD5', app_key=None):
+        """
+        gen sign
+        default sign type 'MD5', testing data from official document
+        >>> params = {'appid': 'wxd930ea5d5a258f4f', 'mch_id': '10000100', 'device_info': 1000,
+        ... 'body': 'test', 'nonce_str': 'ibuaiVcKdpRxkhJA'}
+        >>> app_key = '192006250b4c09247ec02edce69f6a2d'
+        >>> WeChat.gen_sign(params, app_key=app_key)
+        '9A0A8659F005D6984697E2CA0A9CF3B7'
+        """
+        algo_map = {'MD5': hashlib.md5, 'SHA256': hashlib.sha256}
+        ordered_string = WeChat.format_query_param(params, False)
+        raw_string = "{0}&key={1}".format(ordered_string, app_key)
+        sign = algo_map[sign_type](raw_string.encode('utf8')).hexdigest()
+        return sign.upper()
 
+    @staticmethod
+    def to_xml(params):
+        """dict to xml"""
+        xml = ["<xml>"]
+        for k, v in params.items():
+            if v.isdigit():
+                xml.append("<{0}>{1}</{0}>".format(k, v))
+            else:
+                xml.append("<{0}><![CDATA[{1}]]></{0}>".format(k, v))
+        xml.append("</xml>")
+        return "".join(xml)
 
-def gen_sign(params, *, sign_type='MD5', app_key=None):
-    """
-    gen sign
-    default sign type 'MD5', testing data from official document
-    >>> params = {'appid': 'wxd930ea5d5a258f4f', 'mch_id': '10000100', 'device_info': 1000,
-    ... 'body': 'test', 'nonce_str': 'ibuaiVcKdpRxkhJA'}
-    >>> app_key = '192006250b4c09247ec02edce69f6a2d'
-    >>> gen_sign(params, app_key=app_key)
-    '9A0A8659F005D6984697E2CA0A9CF3B7'
-    """
-    algo_map = {'MD5': hashlib.md5, 'SHA256': hashlib.sha256}
-    ordered_string = format_query_param(params, False)
-    raw_string = "{0}&key={1}".format(ordered_string, app_key)
-    sign = algo_map[sign_type](raw_string.encode('utf8')).hexdigest()
-    return sign.upper()
-
-
-def to_xml(params):
-    """dict to xml"""
-    xml = ["<xml>"]
-    for k, v in params.items():
-        if v.isdigit():
-            xml.append("<{0}>{1}</{0}>".format(k, v))
-        else:
-            xml.append("<{0}><![CDATA[{1}]]></{0}>".format(k, v))
-    xml.append("</xml>")
-    return "".join(xml)
-
-
-def to_dict(xml):
-    """xml to dict"""
-    dom_tree = ElementTree.fromstring(xml)
-    data = {node.tag: node.text for node in dom_tree}
-    return data
+    @staticmethod
+    def to_dict(xml):
+        """xml to dict"""
+        dom_tree = ElementTree.fromstring(xml)
+        data = {node.tag: node.text for node in dom_tree}
+        return data
 
 
 if __name__ == '__main__':
--- wxpay/wxpay.py
+++ wxpay/wxpay.py
@@ -1,7 +1,7 @@
 import json
 import time
 from tornado.httpclient import AsyncHTTPClient
-from .utils import to_dict, to_xml, random_str, gen_sign
+from .utils import WeChat
 
 # The following is wechat pay current URL, please check before using.
 UNIFIED_ORDER_URL = 'https://api.mch.weixin.qq.com/pay/unifiedorder'
@@ -34,12 +34,12 @@ class WxPayBasic:
         assert data
         payload = dict()
         payload['method'] = 'POST'
-        payload['body'] = to_xml(data)
+        payload['body'] = WeChat.to_xml(data)
         payload['raise_error'] = False
         if is_ssl:
             payload.update({'cert': (self.cert, self.key)})
         res = await self.request.fetch(url, **payload)
-        result = to_dict(res.body.decode('utf8'))
+        result = WeChat.to_dict(res.body.decode('utf8'))
         return result
 
     async def unified_order(self, **kwargs):
@@ -59,8 +59,8 @@ class WxPayBasic:
         payload["mch_id"] = self.mch_id
         if "spbill_create_ip" not in kwargs:
             payload["spbill_create_ip"] = "127.0.0.1"
-        payload["nonce_str"] = random_str()
-        payload["sign"] = gen_sign(payload, app_key=self.app_key)
+        payload["nonce_str"] = WeChat.random_str()
+        payload["sign"] = WeChat.gen_sign(payload, app_key=self.app_key)
         res = await self._post(UNIFIED_ORDER_URL, data=payload)
         return res
 
@@ -72,8 +72,8 @@ class WxPayBasic:
         payload.update(kwargs)
         payload["appid"] = self.appid  # 公众账号ID
         payload["mch_id"] = self.mch_id  # 商户号
-        payload["nonce_str"] = random_str()
-        payload["sign"] = gen_sign(payload, app_key=self.app_key)
+        payload["nonce_str"] = WeChat.random_str()
+        payload["sign"] = WeChat.gen_sign(payload, app_key=self.app_key)
         return await self._post(ORDER_QUERY_URL, data=payload)
 
     async def close_order(self, **kwargs):
@@ -83,13 +83,13 @@ class WxPayBasic:
         payload.update(kwargs)
         payload["appid"] = self.appid  # 公众账号ID
         payload["mch_id"] = self.mch_id  # 商户号
-        payload["nonce_str"] = random_str()
-        payload["sign"] = gen_sign(payload, app_key=self.app_key)
+        payload["nonce_str"] = WeChat.random_str()
+        payload["sign"] = WeChat.gen_sign(payload, app_key=self.app_key)
         return await self._post(CLOSE_ORDER_URL, data=payload)
 
     def replay(self, msg, ok=True):
         code = "SUCCESS" if ok else "FAIL"
-        return to_xml(dict(return_code=code, return_msg=msg))
+        return WeChat.to_xml(dict(return_code=code, return_msg=msg))
 
     async def prepay_id(self, **kwargs):
         res = await self.unified_order(**kwargs)
@@ -107,9 +107,9 @@ class WxPayApp(WxPayBasic):
         payload['appid'] = self.appid
         payload['partnerid'] = self.mch_id
         payload['package'] = "Sign=WXPay"
-        payload['noncestr'] = random_str()
+        payload['noncestr'] = WeChat.random_str()
         payload['timestamp'] = str(int(time.time()))
-        payload['sign'] = gen_sign(payload, app_key=self.app_key)
+        payload['sign'] = WeChat.gen_sign(payload, app_key=self.app_key)
         if not all(value for value in payload.values()):
             payload = {}
         return payload
@@ -123,10 +123,10 @@ class WxPayJsApi(WxPayBasic):
         js_api_obj = dict()
         js_api_obj["appId"] = self.appid
         js_api_obj["timeStamp"] = int(time.time())
-        js_api_obj["nonceStr"] = random_str()
+        js_api_obj["nonceStr"] = WeChat.random_str()
         js_api_obj["package"] = "prepay_id={0}".format(self.prepay_id(**kwargs))
         js_api_obj["signType"] = "MD5"
-        js_api_obj["paySign"] = gen_sign(js_api_obj, app_key=self.app_key)
+        js_api_obj["paySign"] = WeChat.gen_sign(js_api_obj, app_key=self.app_key)
         return json.dumps(js_api_obj)
 
 
@@ -167,9 +167,9 @@ class WxPayMiniProgram(WxPayBasic):
         again_sign = dict()
         again_sign["appId"] = self.appid  # mini program ID
         again_sign["timeStamp"] = str(int(time.time()))
-        again_sign["nonceStr"] = random_str()
+        again_sign["nonceStr"] = WeChat.random_str()
         again_sign["package"] = "prepay_id={0}".format(prepay_id)
         again_sign["signType"] = "MD5"
-        again_sign["paySign"] = gen_sign(again_sign, app_key=self.app_key)
+        again_sign["paySign"] = WeChat.gen_sign(again_sign, app_key=self.app_key)
         del again_sign["appId"]
         return again_sign
